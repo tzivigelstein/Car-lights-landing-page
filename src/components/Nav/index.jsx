82a86abfab560c79a553fbd290ec3c97cdd9b4a8
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-10-21 10:12:33 -0300

    Add brand name and optimized styles

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -3,6 +3,7 @@ import styles from './index.module.css'
 const Nav = () => {
   return (
     <nav className={styles.nav}>
+      <h1 className={styles.title}>doorj</h1>
       <ul className={styles.navItemList}>
         <li className={styles.listItem}>Inicio</li>
         <li className={styles.listItem}>Productos</li>
