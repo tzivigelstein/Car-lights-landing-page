13e10f68752fe67c7630eca5e1fdd1e25e2fba44
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-10-21 10:10:11 -0300

    Add heading text in about section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -8,12 +8,17 @@ const About = () => {
           <img className={styles.staffSquareImage} src="./assets/pep1.jpg" alt="" />
         </div>
         <div className={styles.staffSquare}>
-          <img className={styles.staffSquareImage} src="./assets/pep2.jpg" alt="" />
+          <img className={styles.staffSquareImage} src="./assets/pep2.png" alt="" />
         </div>
         <div className={styles.staffSquare}>
           <img className={styles.staffSquareImage} src="./assets/pep3.jpg" alt="" />
         </div>
         <div className={styles.staffSquare}></div>
+      </div>
+      <div>
+        <h3>
+          Un poco sobre <span className={styles.accentTitleBrand}>doorj</span>
+        </h3>
       </div>
     </section>
   )
