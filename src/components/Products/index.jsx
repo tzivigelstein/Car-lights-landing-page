224f96503b03b1ab3e426680cf948f60d6faf861
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-10-22 09:27:22 -0300

    Add text to product list

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -6,11 +6,15 @@ import Product from '../Product'
 const Products = () => {
   return (
     <section className={styles.section}>
+      <h3 className={styles.productsSectionTitle}>Linea de productos premium</h3>
       <ul className={styles.productList}>
         {products.map(product => (
           <Product key={product.id} product={product} />
         ))}
       </ul>
+      <p className={styles.productsSectionHelper}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, nulla.
+      </p>
     </section>
   )
 }
