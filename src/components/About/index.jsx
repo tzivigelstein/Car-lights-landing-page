e3101fa93d9d76ff317930c6d90ae31483cc9d08
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-10-22 09:25:54 -0300

    Add about mock text and styling

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -15,10 +15,15 @@ const About = () => {
         </div>
         <div className={styles.staffSquare}></div>
       </div>
-      <div>
-        <h3>
+      <div className={styles.infoContainer}>
+        <h3 className={styles.aboutTitle}>
           Un poco sobre <span className={styles.accentTitleBrand}>doorj</span>
         </h3>
+        <p className={styles.aboutText}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit quibusdam repellat culpa, quisquam ipsa nemo
+          nulla beatae blanditiis dicta est eius. Modi harum doloribus aliquid nostrum neque corrupti dolore ea. Lorem
+          ipsum, dolor sit amet consectetur adipisicing elit. Quae, velit.
+        </p>
       </div>
     </section>
   )
