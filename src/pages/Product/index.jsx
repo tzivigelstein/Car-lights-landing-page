7c829d8a7f12a401ee248050e8f9782a23fd9d71
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-10-31 11:54:01 -0300

    Delete mock and add price parsing

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,32 +1,33 @@
 import styles from './index.module.css'
 import Nav from '../../components/Nav'
 import useProducts from '../../hooks/useProducts'
+import LigthSpot from '../../components/LightSpot'
 
 const Product = () => {
   const { activeProduct } = useProducts()
-  // const { discount, stock, price, title, imageUrl } = activeProduct
+  const { discount, stock, price, title, imageUrl } = activeProduct
 
-  const mock = {
-    id: '2135daad-5c0f-47ba-9848-c5396ca7c2d6',
-    title: 'Set de luz simple',
-    price: (19.99 * 186).toFixed(2),
-    discount: 35,
-    imageUrl: 'car_door_open_light_alert_3.jpg',
-    stock: true,
-  }
-
-  const { discount, stock, price, title, imageUrl } = mock
-
-  const finalPrice = (price - price * 0.1 - (price * discount) / 100).toFixed(2)
+  const finalPrice = (price - price * 0.1 - (price * discount) / 100).toLocaleString([], {
+    maximunSignificantDigits: 2,
+  })
 
   return (
     <div className={styles.container}>
       <Nav />
-      <img className={styles.productImage} src={`../../../assets/${imageUrl}`} alt={title} />
+      <img className={styles.productImageBackground} src={`../../../assets/${imageUrl}`} alt={title} />
+      <div className={styles.gallery}>
+        <img className={styles.productImage} src={`../../../assets/${imageUrl}`} alt={title} />
+      </div>
       <aside className={styles.asideBar}>
+        <LigthSpot position={{ right: 0, bottom: 0 }} />
         <div className={styles.titleAndPrice}>
           <h1 className={styles.productTitle}>{title}</h1>
-          <span className={styles.productPrice}>${price}</span>
+          <span className={styles.productPrice}>
+            $
+            {price.toLocaleString([], {
+              maximunSignificantDigits: 2,
+            })}
+          </span>
         </div>
         <p className={styles.productDescription}>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere ullam impedit officiis illo vel vitae cum
@@ -37,16 +38,26 @@ const Product = () => {
             <h4 className={styles.discountTitle}>Descuentos</h4>
             <div className={styles.discountContainer}>
               <span className={styles.discountCodeText}>
-                Código de descuento <span className={styles.discountCode}>VER2021</span> aplicado 10%
+                Código de descuento <span className={styles.discountCode}>VERANO2021</span> aplicado 10%
               </span>
               <span className={styles.discountCodeText}>
                 Código de descuento <span className={styles.discountCode}>LANZAMIENTO</span> aplicado {discount}%
               </span>
+              <p className={styles.finalPrice}>
+                Precio final despues de descuentos $
+                {finalPrice.toLocaleString([], {
+                  maximunSignificantDigits: 2,
+                })}
+              </p>
             </div>
-            <span style={{ color: 'green' }}>{finalPrice}</span>
           </div>
         </div>
-        <button className={styles.button}>Comprar</button>
+        <button className={styles.button}>
+          Comprar - $
+          {finalPrice.toLocaleString([], {
+            maximunSignificantDigits: 2,
+          })}
+        </button>
         <p className={styles.warningText}>Precio sujeto a impuestos y regulaciones regionales*</p>
       </aside>
     </div>
