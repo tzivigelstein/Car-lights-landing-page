539347398946e690e65011d334d2962506247f8f
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-10-31 12:49:53 -0300

    Add nav buttons

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,16 +1,44 @@
 import styles from './index.module.css'
-import { Link } from 'wouter'
+import { Link, useLocation } from 'wouter'
 
-const Nav = () => {
+const Nav = ({ refs }) => {
+  const [location, setLocation] = useLocation()
+
+  const scrollIntoView = current => {
+    refs[current].current.scrollIntoView({ behavior: 'smooth' })
+  }
+
   return (
     <nav className={styles.nav}>
       <Link href={'/'}>
         <h1 className={styles.title}>doorj</h1>
       </Link>
       <ul className={styles.navItemList}>
-        <li className={styles.listItem}>Inicio</li>
-        <li className={styles.listItem}>Productos</li>
-        <li className={styles.listItem}>Nosotros</li>
+        {location !== '/' ? (
+          <li className={styles.listItem}>
+            <Link href="/">
+              <a className={styles.navLink}>Inicio</a>
+            </Link>
+          </li>
+        ) : (
+          <>
+            <li className={styles.listItem}>
+              <button onClick={() => scrollIntoView(0)} className={styles.navButton}>
+                Inicio
+              </button>
+            </li>
+            <li className={styles.listItem}>
+              <button onClick={() => scrollIntoView(1)} className={styles.navButton}>
+                Productos
+              </button>
+            </li>
+            <li className={styles.listItem}>
+              <button onClick={() => scrollIntoView(2)} className={styles.navButton}>
+                Nosotros
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   )
