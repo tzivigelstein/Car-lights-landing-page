ddd4cc247f57e8dfc598a733ee0046c4f2abe90d
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-10-31 11:52:40 -0300

    Delete fixed decimals

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const products = [
   {
     id: '2135daad-5c0f-47ba-9848-c5396ca7c2d6',
     title: 'Set de luz simple',
-    price: (19.99 * 186).toFixed(2),
+    price: 19.99 * 186,
     discount: 35,
     imageUrl: 'car_door_open_light_alert_3.jpg',
     stock: true,
@@ -10,7 +10,7 @@ const products = [
   {
     id: 'a1468af5-0a7b-46e8-b8e1-ea8da2ecaa53',
     title: 'Set de luces full',
-    price: (59.99 * 186).toFixed(2),
+    price: 59.99 * 186,
     discount: 33,
     imageUrl: 'car_door_open_light_alert_3.jpg',
     selected: true,
@@ -19,10 +19,10 @@ const products = [
   {
     id: 'c72459fc-b2e3-44d0-a8af-6de3858cf0c9',
     title: 'Set de luces doble',
-    price: (49.99 * 186).toFixed(2),
+    price: 49.99 * 186,
     discount: 50,
     imageUrl: 'car_door_open_light_alert_3.jpg',
-    stock: false,
+    stock: true,
   },
 ]
 
