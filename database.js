911b7031ff1c9d37a5b30c827045b513cf640528
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-11-01 12:38:03 -0300

    Add product description

--- database.js
+++ database.js
@@ -6,6 +6,8 @@ const products = [
     discount: 35,
     imageUrl: 'car_door_open_light_alert_3.jpg',
     stock: true,
+    description:
+      'Este pack consta con nuestra increíble alarma puesta sobre la puerta del conductor, es perfecta para la prevención de accidentes, ideal para las noches oscuras',
   },
   {
     id: 'a1468af5-0a7b-46e8-b8e1-ea8da2ecaa53',
@@ -15,6 +17,8 @@ const products = [
     imageUrl: 'car_door_open_light_alert_3.jpg',
     selected: true,
     stock: true,
+    description:
+      'El pack consta de nuestra alarma ubicada en las 4 puertas del vehículo. Incorpora luces antiniebla, ideal para la protección de los niños',
   },
   {
     id: 'c72459fc-b2e3-44d0-a8af-6de3858cf0c9',
@@ -23,6 +27,8 @@ const products = [
     discount: 50,
     imageUrl: 'car_door_open_light_alert_3.jpg',
     stock: true,
+    description:
+      'El pack consta con nuestra alarma ubicada en ambas puertas delantera, además cuenta con luces antiniebla para una mayor precaución',
   },
 ]
 
